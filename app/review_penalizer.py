07a2084b29e9b1ffb16c97ff6ecbe790da97ba4b
meszbogi <bogi.mesz@gmail.com>
2022-09-12 16:41:02 +0200

    Fix review_penalizer

--- app/review_penalizer.py
+++ app/review_penalizer.py
@@ -5,4 +5,4 @@ def calculate_review_penalty(movies):
 
     max_votes = movie_df['votes'].max()
     movie_df['review_penalty'] = movie_df['votes'].apply(lambda v: round((v - max_votes)/100000)*0.1, 1)
-    return movies
+    return movie_df
--- test/test_review_penalizer.py
+++ test/test_review_penalizer.py
@@ -1,4 +1,5 @@
 from app.review_penalizer import calculate_review_penalty
+import pandas as pd
 
 def test_penalty_calculation():
     movies = [{'movieID': '0',
@@ -11,13 +12,13 @@ def test_penalty_calculation():
                'votes': 1000000}
             ]
     result = calculate_review_penalty(movies)
-    result = [{'movieID': '0',
-               'title': 'title_0',
-               'rating': 7.6,
-               'votes': 1200000,
-               'review_penalty': 0},
-               {'movieID': '1',
-               'title': 'title_1',
-               'rating': 7.6,
-               'votes': 1000000,
-               'review_penalty': 0.2}]
+    assert result.equals(pd.DataFrame([{'movieID': '0',
+                        'title': 'title_0',
+                        'rating': 7.6,
+                        'votes': 1200000,
+                        'review_penalty': 0},
+                        {'movieID': '1',
+                        'title': 'title_1',
+                        'rating': 7.6,
+                        'votes': 1000000,
+                        'review_penalty': -0.2}]))
